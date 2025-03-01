65809985485c610f6cab965b44c48e508f0a35bf
lts-pj <lliamsymonds04@gmail.com>
2025-03-01 11:47:53 +1000

    moved secret_key and database_url loading to extensions.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
-from dotenv import load_dotenv
 import os
 
-from extensions import db, bcrypt, login_manager
+from extensions import db, bcrypt, login_manager, DATABASE_URL, SECRET_KEY
 from blueprints.auth import auth
 from blueprints.exercise_handler import exercise_handler
 
@@ -12,12 +11,11 @@ def create_app():
     new_app = Flask(__name__)
     CORS(new_app, supports_credentials=True, origins=["http://localhost:5173"])
 
-    load_dotenv()
 
     #setup database
-    new_app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    new_app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
     new_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    new_app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    new_app.config['SECRET_KEY'] = SECRET_KEY
     new_app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {"pool_pre_ping": True}
     # new_app.config['PERMANENT_SESSION_LIFETIME'] = 604800  # 7 days
     # new_app.config["REMEMBER_COOKIE_DURATION"] = 604800
--- extensions.py
+++ extensions.py
@@ -1,6 +1,14 @@
+import os
+from dotenv import load_dotenv
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+
+load_dotenv()
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
